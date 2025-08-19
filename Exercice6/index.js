9d04610d421a15150707857023e91870604dbf3f
NGnYan <ngnyan@air-de-ngnyan.home>
2025-08-19 19:57:41 +0200

    fix calculateResult function

--- Exercice6/index.js
+++ Exercice6/index.js
@@ -10,15 +10,47 @@ function clearDisplay() {
 
 function calculateResult() {
   try {
-    const result = eval(display.value);
+    const val = display.value;
 
-    if (result === Infinity || result === -Infinity) {
-      display.value = "Division by zero is not allowed";
-    } else if (isNaN(result)) {
-      display.value = "Invalid expression";
-    } else {
-      display.value = result;
+    const regex = /^(-?\d+(?:\.\d+)?)([+\-*/])(-?\d+(?:\.\d+)?)$/;
+
+    const match = val.match(regex);
+
+    if (!match) {
+      display.value = "Invalid input";
+      return;
     }
+
+    const num1 = parseFloat(match[1]);
+    const operator = match[2];
+    const num2 = parseFloat(match[3]);
+
+    let result;
+
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        if (num2 === 0) {
+          display.value = "Division by zero is not allowed";
+          return;
+        } else {
+          result = num1 / num2;
+        }
+        break;
+      default:
+        display.value = "Invalid input";
+        return;
+    }
+
+    display.value = result;
   } catch (error) {
     display.value = "Invalid input";
   }
